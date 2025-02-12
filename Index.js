a67c99ea41bd919178d21d0b245e9ccfaed9bf4f
rinshasherin <rinshasherinck2002@gmail.com>
2025-02-12 14:34:30 +0530

    'learnify'

--- Index.js
+++ Index.js
@@ -4,14 +4,14 @@ const express = require('express')
 const cors = require('cors')
 require('./DB_connection/db')
 const router = require('./Router/router')
-// const jwt = require('./Middlewares/jwtMiddleware')
+
 
 const server = express()
 
 server.use(cors())
 server.use(express.json())
 server.use(router)
-// server.use(jwt)
+ 
 
 server.use('/uploads',express.static('./uploads'))
 
